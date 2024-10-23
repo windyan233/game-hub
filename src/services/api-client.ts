68e33fb3e68d63fad7ec759876cbdcd42b9c40d7
windyan233 <2359034782@qq.com>
2024-10-23 20:15:23 +0800

    Update RAWG API key

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "c7b18323a47d40c394ea5b019646b1f5",
+    key: "b9fe2e4b88f74ae5ae883306dcf65ebf",
   },
 });
